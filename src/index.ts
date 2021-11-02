a0b9767b523c6551d7b6fabbd623a22b3085fe75
Resul <81697915+rturan29@users.noreply.github.com>
2021-11-02 09:20:51 +0300

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import { initializeApp as initializeFirebase } from "@firebase/app";
-import { FirebaseAuth } from "./firebaseAuth";
-import { FirebaseDatabase } from "./firebaseDatabase";
-import { FirestoreDatabase } from "./FirestoreDatabase";
-import { IAuthCallbacks, IPropsDatabase } from "./interfaces";
-
-function useFirestoreDatabase(props?: IPropsDatabase) {
-    return new FirestoreDatabase(props);
-}
-
-function useFirebaseDatabase(props?: IPropsDatabase) {
-    return new FirebaseDatabase(props);
-}
-
-function useFirebaseAuth(props?: IAuthCallbacks) {
-    return new FirebaseAuth(props);
-}
-
-export { initializeFirebase, FirestoreDatabase, FirebaseDatabase, FirebaseAuth };
+export { initializeApp as initializeFirebase } from "@firebase/app";
+export { FirebaseAuth } from "./firebaseAuth";
+export { FirebaseDatabase } from "./firebaseDatabase";
+export { FirestoreDatabase } from "./FirestoreDatabase";
